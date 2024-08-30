dd1b14d445287605f50395a4298e28d193e725df
Bagas Setiaji <stjbagas28@gmail.com>
2024-08-30 14:54:39 +0700

    update

--- client/src/app/api/webhook/route.ts
+++ client/src/app/api/webhook/route.ts
@@ -10,7 +10,7 @@ export async function GET(req: NextRequest) {
 
 async function handleWebhook(req: NextRequest) {
   try {
-    let sender, message :any;
+    let sender, message: any;
 
     if (req.method === 'POST') {
       const body = await req.json();
@@ -23,7 +23,7 @@ async function handleWebhook(req: NextRequest) {
 
     // Proses pesan yang diterima
     console.log('Pesan diterima:', { sender, message });
-    const greetings = ['hi', 'hello','hai','hallo', 'selamat pagi', 'selamat siang', 'selamat sore', 'selamat malam'];
+    const greetings = ['hi', 'hello', 'hai', 'hallo', 'selamat pagi', 'selamat siang', 'selamat sore', 'selamat malam'];
     if (greetings.some(greeting => message.toLowerCase().includes(greeting))) {
       let reply = 'Hai!👋 Saya adalah bot interaktif yang siap membantu Anda 🥰. Saya bisa menjawab pertanyaan Anda tentang pasal undang undang  perlindungan data pribadidengan menggunakan bahasa sehari-hari yang Anda gunakan, yuk tanyakan saja! 🚀';
       await sendReply(sender, reply);
@@ -36,10 +36,29 @@ async function handleWebhook(req: NextRequest) {
     // dijawab oleh flowiseAI
     const response = await flowiseAI(message);
 
+    if (response.text == 'Tidak ada hasil yang ditemukan dalam database.' || response.text == 'Jawaban: Tidak ada data yang ditemukan untuk isi dari data pribadi.') {
+      const response = await flowiseAIFromCSV(message);
+      const reply = response.text;
+      if (reply == 'hemm , iam not sure') {
+        const answer = 'Maaf, saya tidak mengerti pertanyaan Anda. Silakan coba lagi. 🤔';
+        await sendReply(sender, answer);
+        return NextResponse.json({
+          success: true,
+          reply: reply
+        });
+      }
+
+      await sendReply(sender, reply);
+      return NextResponse.json({
+        success: true,
+        reply: reply
+      });
+    }
+
     const reply = response.text;
 
     const sendReplyResponse = await sendReply(sender, reply);
-    
+
 
     return NextResponse.json({ success: true, sendReplyResponse });
   } catch (error) {
@@ -77,6 +96,25 @@ async function flowiseAI(input: string) {
     },
     body: JSON.stringify({
       question: input,
+    }),
+  });
+
+  return responses.json();
+}
+
+// csv file flowiseAI
+async function flowiseAIFromCSV(input: string) {
+  const url = 'https://flowiseai-railway-production-9629.up.railway.app/api/v1/prediction/598d0480-372a-4a3f-a376-fc832fa1ef26';
+
+  const responses = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      "question": input,
+      "chatId": "ca834cec-9f97-4ea8-a929-3a656228aca3",
+      "socketIOClientId": "vezTJdZz_H9JRb5QAAHt"
     }),
   });
 
