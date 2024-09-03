f1da01703fd3205ebe3c86e6b4f6e475226654c6
Bagas Setiaji <stjbagas28@gmail.com>
2024-09-03 13:50:13 +0700

    update

--- client/src/app/api/webhook-rs/route.ts
+++ client/src/app/api/webhook-rs/route.ts
@@ -11,6 +11,8 @@ export async function GET(req: NextRequest) {
 
 async function handleWebhook(req: NextRequest) {
     try {
+        const systemMessagePrompt2 = 'Anda adalah seorang asisten AI yang sangat ahli dalam memberikan informasi medis dan pengetahuan tentang berbagai kondisi penyakit. Anda hanya dapat menjawab pertanyaan yang terkait dengan kondisi penyakit, pengobatan, gejala, penyebab, dan perawatan yang relevan. Anda tidak dapat menjawab pertanyaan yang berada di luar lingkup medis dan kesehatan. Setiap pertanyaan yang diajukan oleh pengguna tentang kondisi penyakit akan dicatat dan disimpan dalam memori Anda, memungkinkan Anda untuk merujuk ke pertanyaan sebelumnya guna memberikan jawaban yang lebih akurat dan sesuai dengan konteks pertanyaan baru yang terkait. \n Contoh penggunaan: \n •	Pengguna dapat menanyakan gejala, penyebab, atau pengobatan dari suatu penyakit. \n•	Anda dapat memberikan informasi tentang langkah-langkah pencegahan, perawatan mandiri, atau kapan harus mencari bantuan medis profesional. \n•	Anda dapat menjelaskan perbedaan antara kondisi-kondisi yang sering disalahpahami atau memberikan saran umum berdasarkan pengetahuan medis yang terpercaya. jika terdapat pertanyaan yang tidak relevan , tolong berikan jawaban "maaf untuk saat ini saya hanya bisa menjawab tentang kesehatan atau kondisi medis . Jika anda ingin mengganti menu , tolong ketikan `start` . Terimakasih"';
+        const ststemMessagePrompt4 = "Anda adalah asisten AI yang sangat berpengetahuan luas dalam hal asuransi kesehatan, termasuk BPJS dan berbagai jenis asuransi kesehatan lainnya. Anda hanya dapat menjawab pertanyaan yang terkait dengan informasi tentang BPJS, asuransi kesehatan, cakupan layanan, prosedur klaim, dan manfaat yang tersedia. Anda tidak dapat menjawab pertanyaan di luar lingkup asuransi kesehatan. Setiap pertanyaan yang diajukan oleh pengguna mengenai BPJS atau asuransi kesehatan akan dicatat dan disimpan dalam memori Anda, memungkinkan Anda merujuk ke pertanyaan sebelumnya untuk memberikan jawaban yang lebih akurat dan sesuai dengan konteks pertanyaan baru yang terkait. \n Contoh penggunaan:\n •	Pengguna dapat menanyakan tentang jenis layanan yang ditanggung oleh BPJS atau asuransi kesehatan tertentu.\n •	Anda dapat memberikan informasi tentang prosedur pendaftaran BPJS atau asuransi kesehatan lainnya, termasuk syarat dan ketentuannya.\n •	Anda dapat menjelaskan perbedaan antara BPJS dan asuransi kesehatan swasta, serta kelebihan dan kekurangan masing-masing.\n •	Anda dapat memberikan panduan tentang cara mengajukan klaim, dokumen yang diperlukan, dan proses klaim asuransi.\n •	Anda dapat menjawab pertanyaan tentang biaya premi, jangkauan perlindungan, serta cara memilih asuransi kesehatan yang sesuai dengan kebutuhan.  jika terdapat pertanyaan yang tidak relevan , tolong berikan jawaban 'maaf untuk saat ini saya hanya bisa menjawab tentang kesehatan atau kondisi medis . Jika anda ingin mengganti menu , tolong ketikan `start` . Terimakasih'"
         let sender, message: any;
 
         if (req.method === 'POST') {
@@ -22,16 +24,18 @@ async function handleWebhook(req: NextRequest) {
             message = params.get('message');
         }
 
-        console.log('Pesan diterima:', { sender, message }, redisClient.get(sender));
+        console.log('Pesan diterima:', { sender, message }, await redisClient.get(sender));
         const greetings = ['hi', 'hello', 'hai', 'hallo', 'selamatpagi', 'selamatsiang', 'selamatsore', 'selamatmalam', 'start'];
         const menuText = ['registrasirawatjalan', 'riwayatmedis', 'penjadwalankonsultasi', 'bpjsdanasuransi', 'pembayarandanpenagihan'];
 
         if (message != null) {
             // Proses pesan Starting
             if (greetings.some(greeting => message.toLowerCase().replace(/\s+/g, '').includes(greeting))) {
+                await redisClient.del(sender);
+                await redisClient.del(sender + "_menu");
                 let reply = "Hai!👋 Saya adalah bot interaktif yang siap membantu Anda 😁. Silahkan pilih menu unutk mengakses fiture dari bot interaktif ini : \n \n   *1. Registrasi Rawat Jalan* \n   *2. Riwayat Medis*\n   *3. Penjadwalan Konsultasi*\n   *4. BPJS dan Asuransi* \n   *5. Pembayaran dan Penagihan*  \n \nAnda Hanya perlu menginputkan nomor menu atau ketik menu tersebut sebagai contoh `2` atau `Riwayat Medis`   \n \n  *Terimakasih* 🥰";
-                await redisClient.set(sender, '1'); // Set message to 1
-                console.log('Set message to 1 for sender:', sender);
+                await redisClient.set(sender, 'initial'); // Set message to initial
+                console.log('Set message to initial for sender:', sender);
                 await sendReply(sender, reply);
                 return NextResponse.json({
                     success: true,
@@ -40,9 +44,10 @@ async function handleWebhook(req: NextRequest) {
             }
 
             // Proses Input NIK
-            if (message == '2' || menuText.some(menu => message.toLowerCase().replace(/\s+/g, '').includes(menu))) {
-                let reply = "Anda telah memilih menu " + message + " \n Tolong Inputkan NIK anda untuk mengakses fitur tersebut";
+            if (message == '1' || message == '2' || message == '3' || message == '4' || message == '5' || menuText.some(menu => message.toLowerCase().replace(/\s+/g, '').includes(menu))) {
+                let reply = "Anda telah memilih menu " + message + " \nTolong Inputkan NIK anda untuk mengakses fitur tersebut";
                 await redisClient.set(sender, 'nik_done');
+                await redisClient.set(sender + "_menu", message);
                 console.log('Set message to nik_done for sender:', sender);
                 await sendReply(sender, reply);
                 return NextResponse.json({
@@ -52,12 +57,15 @@ async function handleWebhook(req: NextRequest) {
             }
 
             // cek nik ke databse
+            // RIWAYAT MEDIS
             const storedMessage = await redisClient.get(sender);
-            console.log('Stored message:', storedMessage);
-            if (storedMessage == 'nik_done') {
-                const response = await flowiseAI(message);
+            const storedMenu = await redisClient.get(sender + "_menu");
+            console.log('Stored message:', storedMessage, storedMenu);
+            if (storedMessage == 'nik_done' && (storedMenu == '2' || storedMenu == 'riwayatmedis')) {
+                const systemMessagePrompt = 'Given an input question, first construct a syntactically correct {dialect} query to run, then look at the query results and return the answer. Unless the user specifies in their question a specific number of examples they want to get, always limit your query to a maximum of {top_k} results. You can sort the results by relevant columns to return the most interesting examples in the database.the given data is patient data with their disease, \n if the user asks about the disease they are suffering from, answer by referring to the disease columnif there is a question "sebutkan biodata dari {input}", then answer with an example "Berdasarkan NIK 330111, nama anda adalah Ali Nasgor, mempunyai nomor bpjs ketenagakerjaan 001135 dengan masa berlaku sampai 2024-06-23" . If the question relates to information that is not in this database, answer with "No results found in the database." \n Never ask for all columns from a given table, ask for only a few columns that are relevant to the question.Be careful to only use column names that you can see in the schema description. Be careful not to ask for columns that do not exist. Also pay attention to which columns are in which tables.Please answer in Indonesian \n Use the following format:\nQuestion: "Question here"\nSQLQuery: "SQL query to be executed"\n SQLResult: "Result of SQLQuery\n"Answer: "Final answer here"Use only the tables listed below.\n{table_info}\nQuestion: {input}'
+                const response = await flowiseAI(message, systemMessagePrompt, 'SampleDataPasien');
                 console.log('Response flow 1:', response);
-                if (response.text == 'Tidak ada hasil yang ditemukan dalam database.') {
+                if (response.text == 'Tidak ada hasil yang ditemukan dalam database.' || response.text == 'No results found in the database.') {
                     await sendReply(sender, 'Maaf , Untuk saat ini data yang anda masukan belum ada di sistem kami 😔');
                     return NextResponse.json({
                         success: true,
@@ -67,28 +75,57 @@ async function handleWebhook(req: NextRequest) {
                 await redisClient.set(sender, 'biodata_done');
                 console.log('Set message to biodata_done for sender:', sender);
                 await sendReply(sender, response.text);
+                await flowiseAIGeneral(response.text.replace('anda', 'aku'), systemMessagePrompt2);
+                return NextResponse.json({
+                    success: true,
+                    reply: response
+                });
+            }
+
+            // BPJS DAN ASURANSI
+            if (storedMessage == 'nik_done' && (storedMenu == '4' || storedMenu == 'bpjsdanasuransi')) {
+                const systemMessagePrompt = 'Given an input question, first construct a syntactically correct {dialect} query to run, then look at the query results and return the answer. Unless the user specifies in their question a specific number of examples they want to get, always limit your query to a maximum of {top_k} results. You can sort the results by relevant columns to return the most interesting examples in the database.the given data is patient data with their disease, \n if there is a question "sebutkan biodata dari {input}", then answer with an example "Berdasarkan NIK 330111, nama anda adalah Ali Nasgor, anda memiliki riwayat penyakit asma, adakah yang ingin anda tanyakan dengan penyakit asma" . If the question relates to information that is not in this database, answer with "No results found in the database." \n Never ask for all columns from a given table, ask for only a few columns that are relevant to the question.Be careful to only use column names that you can see in the schema description. Be careful not to ask for columns that do not exist. Also pay attention to which columns are in which tables.Please answer in Indonesian \n Use the following format:\nQuestion: "Question here"\nSQLQuery: "SQL query to be executed"\n SQLResult: "Result of SQLQuery\n"Answer: "Final answer here"Use only the tables listed below.\n{table_info}\nQuestion: {input}'
+                const response = await flowiseAI(message, systemMessagePrompt, 'SampleDataBPJSAsuransi');
+                console.log('Response flow 1:', response);
+                if (response.text == 'Tidak ada hasil yang ditemukan dalam database.' || response.text == 'No results found in the database.') {
+                    await sendReply(sender, 'Maaf , Untuk saat ini data yang anda masukan belum ada di sistem kami 😔');
+                    return NextResponse.json({
+                        success: true,
+                        reply: response
+                    });
+                }
+                await redisClient.set(sender, 'biodata_done');
+                console.log('Set message to biodata_done for sender:', sender);
+                await sendReply(sender, response.text);
+                await flowiseAIGeneral(response.text.replace('anda', 'aku'), ststemMessagePrompt4);
                 return NextResponse.json({
                     success: true,
                     reply: response
                 });
             }
         }
-
-        // dijawab oleh flowiseAI
-        const response = await flowiseAIGeneral(message);
-
-        if (response.text == 'Tidak ada hasil yang ditemukan dalam database.' || response.text == 'Jawaban: Tidak ada data yang ditemukan untuk isi dari data pribadi.') {
-            const response = await flowiseAIFromCSV(message);
-            const reply = response.text;
-            if (reply == 'hemm , iam not sure') {
-                const answer = 'Maaf, saya tidak mengerti pertanyaan Anda. Silakan coba lagi. 🤔';
-                await sendReply(sender, answer);
+        const storedMessage = await redisClient.get(sender);
+        const storedMenu = await redisClient.get(sender+"_menu");
+        if (storedMessage == 'biodata_done') {
+            // dijawab oleh flowiseAI
+            let prompt :any = systemMessagePrompt2
+            if(storedMenu == '2' || storedMenu == 'riwayatmedis'){
+                prompt = systemMessagePrompt2
+            } else {
+                prompt = ststemMessagePrompt4
+            }
+            let response :any = await flowiseAIGeneral(message, prompt );
+    
+            if (response.text == 'Tidak ada hasil yang ditemukan dalam database.' || response.text == 'Jawaban: Tidak ada data yang ditemukan untuk isi dari data pribadi.') {
+                const reply = response.text;
+                await sendReply(sender, reply);
                 return NextResponse.json({
                     success: true,
                     reply: reply
                 });
             }
 
+            const reply = response.text;
             await sendReply(sender, reply);
             return NextResponse.json({
                 success: true,
@@ -96,9 +133,8 @@ async function handleWebhook(req: NextRequest) {
             });
         }
 
-        const reply = response.text;
 
-        const sendReplyResponse = await sendReply(sender, reply);
+        const sendReplyResponse = await sendReply(sender, 'maaf , mungkin anda salah pilih menu , silahkan ketik `start` untuk memulai ulang');
 
         return NextResponse.json({ success: true, sendReplyResponse });
     } catch (error) {
@@ -126,7 +162,7 @@ async function sendReply(to: string, message: string) {
     return response.json();
 }
 
-async function flowiseAIGeneral(input: string) {
+async function flowiseAIGeneral(input: string, systemMessagePrompt: string) {
     const url = 'https://flowiseai-railway-production-9629.up.railway.app/api/v1/prediction/c6ff5c51-b0d5-4875-a994-463ed49f0b25';
 
     const responses = await fetch(url, {
@@ -135,15 +171,15 @@ async function flowiseAIGeneral(input: string) {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-            chatId: "77cf063c-6ed6-4592-bdc8-b89a0a162af9",
             question: input,
+            systemMessagePrompt: systemMessagePrompt
         }),
     });
 
     return responses.json();
 }
 
-async function flowiseAI(input: string) {
+async function flowiseAI(input: string, systemMessagePrompt: string, tablle: string) {
     const url = 'https://flowiseai-railway-production-9629.up.railway.app/api/v1/prediction/8c1c3efc-a126-46dd-8f44-63b233494d46';
 
     const responses = await fetch(url, {
@@ -153,6 +189,8 @@ async function flowiseAI(input: string) {
         },
         body: JSON.stringify({
             question: `sebutkan biodata dari nik ${input}`,
+            includesTables: tablle,
+            customPrompt: systemMessagePrompt
         }),
     });
 
@@ -170,8 +208,6 @@ async function flowiseAIFromCSV(input: string) {
         },
         body: JSON.stringify({
             "question": input,
-            "chatId": "ca834cec-9f97-4ea8-a929-3a656228aca3",
-            "socketIOClientId": "vezTJdZz_H9JRb5QAAHt"
         }),
     });
 
